ba3a84f94609e11d6f400b63f5676366bd66d53b
lesi97 <52697790+lesi97@users.noreply.github.com>
2023-05-20 20:16:27 +0100

    Update beloved.php

--- mendez/beloved.php
+++ mendez/beloved.php
@@ -1,8 +1,8 @@
 <?php
 
 //	Variables
-	
-  $api_key = getenv("BUNGIE_KEY");
+
+	$api_key = getenv("BUNGIE_KEY");
 
 	$destinyMembershipId = '4611686018506946834/';
 	$membershipType = '3/';
@@ -31,19 +31,19 @@
 
 //	Warlock
 
-    $chWarlock = curl_init();
-    curl_setopt($chWarlock, CURLOPT_URL, $urlWarlock);
-    curl_setopt($chWarlock, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($chWarlock, CURLOPT_HTTPHEADER, array(
-        'x-api-key: ' . $api_key
-    ));
-    $response = curl_exec($chWarlock);
-    curl_close($chWarlock);
-    $current_dateTime = date("Y-m-d H:i:s");
+	$chWarlock = curl_init();
+	curl_setopt($chWarlock, CURLOPT_URL, $urlWarlock);
+	curl_setopt($chWarlock, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($chWarlock, CURLOPT_HTTPHEADER, array(
+		'x-api-key: ' . $api_key
+	));
+	$response = curl_exec($chWarlock);
+	curl_close($chWarlock);
+	$current_dateTime = date("Y-m-d H:i:s");
 
-    if ($response !== false) {
-        $data = json_decode($response, true);
-        $gunTracker = $data["Response"]["itemComponents"]["plugObjectives"]["data"][$weapon]["objectivesPerPlug"][$crucibleTracker]["0"]["progress"];
+	if ($response !== false) {
+		$data = json_decode($response, true);
+		$gunTracker = $data["Response"]["itemComponents"]["plugObjectives"]["data"][$weapon]["objectivesPerPlug"][$crucibleTracker]["0"]["progress"];
 		if ($gunTracker !== null) {						
 			$jsonData = file_get_contents($jsonFileName);
 			$data1 = json_decode($jsonData, true);			
@@ -57,19 +57,19 @@
 
 //	Hunter
 
-    $chHunter = curl_init();
-    curl_setopt($chHunter, CURLOPT_URL, $urlHunter);
-    curl_setopt($chHunter, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($chHunter, CURLOPT_HTTPHEADER, array(
-        'x-api-key: ' . $api_key
-    ));
-    $response = curl_exec($chHunter);
-    curl_close($chHunter);
-    $current_dateTime = date("Y-m-d H:i:s");
+	$chHunter = curl_init();
+	curl_setopt($chHunter, CURLOPT_URL, $urlHunter);
+	curl_setopt($chHunter, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($chHunter, CURLOPT_HTTPHEADER, array(
+		'x-api-key: ' . $api_key
+	));
+	$response = curl_exec($chHunter);
+	curl_close($chHunter);
+	$current_dateTime = date("Y-m-d H:i:s");
 
-    if ($response !== false) {
-        $data = json_decode($response, true);
-        $gunTracker = $data["Response"]["itemComponents"]["plugObjectives"]["data"][$weapon]["objectivesPerPlug"][$crucibleTracker]["0"]["progress"];
+	if ($response !== false) {
+		$data = json_decode($response, true);
+		$gunTracker = $data["Response"]["itemComponents"]["plugObjectives"]["data"][$weapon]["objectivesPerPlug"][$crucibleTracker]["0"]["progress"];
 		if ($gunTracker !== null) {			
 			$jsonData = file_get_contents($jsonFileName);
 			$data1 = json_decode($jsonData, true);			
@@ -83,18 +83,18 @@
 
 //	Titan
 
-    $chTitan = curl_init();
-    curl_setopt($chTitan, CURLOPT_URL, $urlTitan);
-    curl_setopt($chTitan, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($chTitan, CURLOPT_HTTPHEADER, array(
-        'x-api-key: ' . $api_key
-    ));
-    $response = curl_exec($chTitan);
-    curl_close($chTitan);
-    $current_dateTime = date("Y-m-d H:i:s");
-    if ($response !== false) {
-        $data = json_decode($response, true);
-        $gunTracker = $data["Response"]["itemComponents"]["plugObjectives"]["data"][$weapon]["objectivesPerPlug"][$crucibleTracker]["0"]["progress"];
+	$chTitan = curl_init();
+	curl_setopt($chTitan, CURLOPT_URL, $urlTitan);
+	curl_setopt($chTitan, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($chTitan, CURLOPT_HTTPHEADER, array(
+		'x-api-key: ' . $api_key
+	));
+	$response = curl_exec($chTitan);
+	curl_close($chTitan);
+	$current_dateTime = date("Y-m-d H:i:s");
+	if ($response !== false) {
+		$data = json_decode($response, true);
+		$gunTracker = $data["Response"]["itemComponents"]["plugObjectives"]["data"][$weapon]["objectivesPerPlug"][$crucibleTracker]["0"]["progress"];
 		if ($gunTracker !== null) {						
 			$jsonData = file_get_contents($jsonFileName);
 			$data1 = json_decode($jsonData, true);			
